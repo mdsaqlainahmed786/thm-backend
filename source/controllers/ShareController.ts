8160fa731decd0042f33e897e842780985d862ee
badolakiran96 <kiran.appcrunk@gmail.com>
2024-11-21 19:01:46 +0530

    fix: feat add post sharer api

--- source/controllers/ShareController.ts
+++ source/controllers/ShareController.ts
@@ -1,17 +1,82 @@
 import { Request, Response, NextFunction } from "express";
-import { httpInternalServerError } from "../utils/response";
+import { httpInternalServerError, httpNotFoundOr404, httpCreated, httpNoContent } from "../utils/response";
 import { ErrorMessage } from "../utils/response-message/error";
+import crypto from 'crypto';
+import Like from "../database/models/like.model";
+import SavedPost from "../database/models/savedPost.model";
+import EventJoin from "../database/models/eventJoin.model";
+import { fetchPosts } from "../database/models/post.model";
+import User from "../database/models/user.model";
+import SharedContent from "../database/models/sharedContent.model";
+import { ObjectId } from "mongodb";
+import { ContentType } from "../common";
 const posts = async (request: Request, response: Response, next: NextFunction) => {
     try {
-        const { thmsh } = request.query;
-        console.log(thmsh);
-
-
-
-        return response.send("post")
+        const id = request.user?.id;
+        const { postID, userID } = request.query;
+        if (!postID || !userID) {
+            return response.send(httpNotFoundOr404({}));
+        }
+        const decryptedPostID = decrypt(postID as string);
+        const decryptedUserID = decrypt(userID as string);
+        const [likedByMe, savedByMe, joiningEvents, user] = await Promise.all([
+            Like.distinct('postID', { userID: id, postID: { $ne: null } }),
+            SavedPost.distinct('postID', { userID: id, postID: { $ne: null } }),
+            EventJoin.distinct('postID', { userID: id, postID: { $ne: null } }),
+            User.findOne({ _id: decryptedUserID }),
+        ]);
+        const [post, isSharedBefore,] = await Promise.all([
+            fetchPosts({ _id: new ObjectId(decryptedPostID) }, likedByMe, savedByMe, joiningEvents, 1, 1),
+            SharedContent.findOne({ contentID: decryptedPostID, userID: decryptedUserID, contentType: ContentType.POST }),
+        ])
+        if (!post || post?.length === 0) {
+            return response.send(httpNotFoundOr404(ErrorMessage.invalidRequest("Post not found"), "Post not found"));
+        }
+        if (!user) {
+            return response.send(httpNotFoundOr404(ErrorMessage.invalidRequest(ErrorMessage.USER_NOT_FOUND), ErrorMessage.USER_NOT_FOUND));
+        }
+        if (!isSharedBefore) {
+            const newSharedContent = new SharedContent();
+            newSharedContent.contentID = decryptedPostID;
+            newSharedContent.contentType = ContentType.POST;
+            newSharedContent.userID = user.id;//Shared By
+            newSharedContent.businessProfileID = user.businessProfileID ?? null;
+            await newSharedContent.save();
+            return response.send(httpCreated(post[0], "Content shared successfully"));
+        }
+        return response.send(httpNoContent(post[0], 'Content shared successfully'));
     } catch (error: any) {
         next(httpInternalServerError(error, error.message ?? ErrorMessage.INTERNAL_SERVER_ERROR));
     }
 }
 
-export default { posts }
+const tester = async (request: Request, response: Response, next: NextFunction) => {
+    const userAgent = request.headers['user-agent'];
+    return response.send({
+        userAgent,
+    })
+}
+
+const SECRET_KEY = '1234567890123456'; // Same key as in Java
+const IV = '1234567890123456'; // Same IV as in Java
+const ALGORITHM = 'aes-128-cbc';
+function encrypt(data: string) {
+    const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(SECRET_KEY), Buffer.from(IV));
+    let encrypted = cipher.update(data, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+
+    // URL-safe Base64 encoding
+    return encrypted.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+function decrypt(encryptedData: string) {
+    // Convert URL-safe Base64 back to standard Base64
+    const base64 = encryptedData.replace(/-/g, '+').replace(/_/g, '/');
+
+    const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(SECRET_KEY), Buffer.from(IV));
+    let decrypted = decipher.update(base64, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+export default { posts, tester }
--- source/routes/v1/share.ts
+++ source/routes/v1/share.ts
@@ -3,4 +3,5 @@ import SearchController from "../../controllers/SearchController";
 import ShareController from "../../controllers/ShareController";
 const ShareEndpoints: Router = express.Router();
 ShareEndpoints.get('/posts', ShareController.posts);
+ShareEndpoints.get('/tester', ShareController.tester);
 export default ShareEndpoints;
