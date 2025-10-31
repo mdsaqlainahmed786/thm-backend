0a0f98c21d37f3bb12f77ba9b360bec5d130ec0a
Saqlain <riyanahmed1703@gmail.com>
2025-10-31 15:07:01 +0530

    working successfully aws SES

--- source/quicktest.ts
+++
@@ -1,7 +0,0 @@
-// quick-test.ts (temp)
-import EmailNotificationService from "./services/EmailNotificationService";
-(async () => {
-  const svc = new EmailNotificationService();
-  await svc.sendEmailOTP(123456, "cartcrazeofficial786@gmail.com", "verify-email");
-  console.log("Sent.");
-})();
