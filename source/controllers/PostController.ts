496b62e6b13c56d445bc698e5d1bb68cd858f447
Ubuntu <ubuntu@ip-172-31-34-140.ap-south-1.compute.internal>
2025-10-31 14:51:29 +0000

    decreased latency of the store function in posts

--- source/controllers/PostController.ts
+++ source/controllers/PostController.ts
@@ -43,165 +43,173 @@ const MAX_CONTENT_LENGTH = 20; // Set maximum content length
 const MAX_CONTENT_UPLOADS = 2;
 const MAX_VIDEO_UPLOADS = 1;
 const MAX_IMAGE_UPLOADS = 2;
+
 const store = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-        const { id, accountType, businessProfileID } = request.user;
-        const { content, placeName, lat, lng, tagged, feelings } = request.body;
-        const files = request.files as { [fieldname: string]: Express.Multer.File[] };
-        // const images = files && files.images as Express.Multer.S3File[];
-        // const videos = files && files.videos as Express.Multer.S3File[];
-        const mediaFiles = files && files.media as Express.Multer.S3File[];
-        const images = mediaFiles && mediaFiles.filter((file) => file.mimetype.startsWith('image/'));
-        const videos = mediaFiles && mediaFiles.filter((file) => file.mimetype.startsWith('video/'));
+  try {
+    const { id, accountType, businessProfileID } = request.user;
+    const { content, placeName, lat, lng, tagged, feelings } = request.body;
+    const files = request.files as { [fieldname: string]: Express.Multer.File[] };
+    const mediaFiles = files && files.media as Express.Multer.S3File[] | undefined;
+    const images = (mediaFiles || []).filter((file) => file.mimetype.startsWith('image/'));
+    const videos = (mediaFiles || []).filter((file) => file.mimetype.startsWith('video/'));
 
-        const authUser = await User.findOne({ _id: id });
-        if (!authUser) {
-            return response.send(httpNotFoundOr404(ErrorMessage.invalidRequest(ErrorMessage.USER_NOT_FOUND), ErrorMessage.USER_NOT_FOUND));
-        }
-        if (!content && !mediaFiles) {
-            return response.send(httpBadRequest(ErrorMessage.invalidRequest("Content is required for creating a post"), 'Content is required for creating a post'))
-        }
-        /**
-         * Content restrictions for individual user
-         */
-        const today = new Date();
-        const startOfDay = new Date(today.setHours(0, 0, 0, 0));
-        const endOfDay = new Date(today.setHours(23, 59, 59, 999));
-        const [haveSubscription, dailyContentLimit] = await Promise.all([
-            hasActiveSubscription(id),
-            DailyContentLimit.findOne({
-                userID: id, timeStamp: {
-                    $gte: startOfDay,
-                    $lt: endOfDay
-                }
-            })
-        ]);
-        if (authUser.accountType === AccountType.INDIVIDUAL) {
-            if (!haveSubscription) {
-                if (!dailyContentLimit && content && countWords(content) > MAX_CONTENT_LENGTH) {
-                    const error = `Content must be a string and cannot exceed ${MAX_CONTENT_LENGTH} words.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-                } else if (!dailyContentLimit && images && images.length > MAX_IMAGE_UPLOADS) {
-
-                    await deleteUnwantedFiles(images);
-                    await deleteUnwantedFiles(videos);
-                    const error = `You cannot upload multiple images because your current plan does not include this feature.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-
-                } else if (!dailyContentLimit && videos && videos.length > MAX_VIDEO_UPLOADS) {
-                    await deleteUnwantedFiles(images);
-                    await deleteUnwantedFiles(videos);
-                    const error = `You cannot upload multiple videos because your current plan does not include this feature.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-
-                } else if (dailyContentLimit && dailyContentLimit.text >= MAX_CONTENT_UPLOADS && content && content !== "") {
-
-                    const error = `Your daily content upload limit has been exceeded. Please upgrade your account to avoid this error.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-
-                } else if (dailyContentLimit && dailyContentLimit.images !== 0 && images && images.length >= dailyContentLimit.images) {
-
-                    await deleteUnwantedFiles(images);
-                    await deleteUnwantedFiles(videos);
-                    const error = `Your daily image upload limit has been exceeded. Please upgrade your account to avoid this error.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-
-                } else if (dailyContentLimit && dailyContentLimit.videos !== 0 && videos && videos.length >= dailyContentLimit.videos) {
-                    await deleteUnwantedFiles(images);
-                    await deleteUnwantedFiles(videos);
-                    const error = `Your daily video upload limit has been exceeded. Please upgrade your account to avoid this error.`;
-                    return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error))
-                }
-            }
-        }
-        const newPost = new Post();
-        if (accountType === AccountType.BUSINESS && businessProfileID) {
-            newPost.businessProfileID = businessProfileID;
-        }
-        newPost.postType = PostType.POST;
-        newPost.userID = id;
-        newPost.isPublished = true;
-        newPost.content = content;
-        newPost.feelings = feelings ?? "";
-        if (tagged && isArray(tagged)) {
-            newPost.tagged = tagged;
-        } else {
-            newPost.tagged = [];
-        }
-
-        if (placeName && lat && lng) {
-            newPost.location = { placeName, lat, lng };
-        } else {
-            newPost.location = null;
-        }
-        if (lat && lng) {
-            newPost.geoCoordinate = { type: "Point", coordinates: [lng, lat] };
-        } else if (authUser && authUser.geoCoordinate) {
-            newPost.geoCoordinate = authUser.geoCoordinate;
-        } else {
-            newPost.geoCoordinate = { type: "Point", coordinates: lat_lng };
-        }
-
-        /**
-         * Handle post media
-         */
-        let mediaIDs: MongoID[] = []
-        // if (videos && videos.length !== 0 || images && images.length !== 0) {
-        //     const [videoList, imageList] = await Promise.all([
-        //         storeMedia(videos, id, businessProfileID, MediaType.VIDEO, AwsS3AccessEndpoints.POST, 'POST'),
-        //         storeMedia(images, id, businessProfileID, MediaType.IMAGE, AwsS3AccessEndpoints.POST, 'POST'),
-        //     ])
-        //     if (imageList && imageList.length !== 0) {
-        //         imageList.map((image) => mediaIDs.push(image.id));
-        //     }
-        //     if (videoList && videoList.length !== 0) {
-        //         videoList.map((video) => mediaIDs.push(video.id));
-        //     }
-        // }
-        if (mediaFiles && mediaFiles.length !== 0) {
-            const mediaList = await storeMedia(mediaFiles, id, businessProfileID, AwsS3AccessEndpoints.POST, 'POST');
-            if (mediaList && mediaList.length !== 0) {
-                mediaList.map((media) => mediaIDs.push(media.id));
-            }
-        }
-        newPost.media = mediaIDs;
-        const savedPost = await newPost.save();
-        //@ts-ignore
-        UserRecentPostCache.set(request.user.id, newPost._id.toString());
-        FeedOrderCache.clear(request.user.id);
-
-        if (savedPost && savedPost.tagged && savedPost.tagged.length !== 0) {
-            savedPost.tagged.map((tagged) => {
-                AppNotificationController.store(id, tagged, NotificationType.TAGGED, { postID: savedPost.id, userID: tagged }).catch((error) => console.error(error));
-            });
-        }
-
-        /**
-         * Only for individual account 
-         */
-        if (savedPost && !haveSubscription && accountType === AccountType.INDIVIDUAL) {
-            if (!dailyContentLimit) {
-                const today = new Date();
-                const midnightToday = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
-                const newDailyContentLimit = new DailyContentLimit();
-                newDailyContentLimit.timeStamp = midnightToday;
-                newDailyContentLimit.userID = id;
-                newDailyContentLimit.videos = (videos && videos.length) ? videos.length : 0;
-                newDailyContentLimit.images = (images && images.length) ? images.length : 0;
-                newDailyContentLimit.text = (content && content !== "") ? 1 : 0;
-                await newDailyContentLimit.save();
-            } else {
-                dailyContentLimit.videos = (videos && videos.length) ? dailyContentLimit.videos + videos.length : dailyContentLimit.videos;
-                dailyContentLimit.images = (images && images.length) ? dailyContentLimit.images + images.length : dailyContentLimit.images;
-                dailyContentLimit.text = (content && content !== "") ? dailyContentLimit.text + 1 : dailyContentLimit.text;
-                await dailyContentLimit.save();
-            }
-        }
-        return response.send(httpCreated(savedPost, 'Your post has been created successfully'));
-    } catch (error: any) {
-        next(httpInternalServerError(error, error.message ?? ErrorMessage.INTERNAL_SERVER_ERROR));
+    if (!id) {
+      return response.send(httpNotFoundOr404(
+        ErrorMessage.invalidRequest(ErrorMessage.USER_NOT_FOUND),
+        ErrorMessage.USER_NOT_FOUND
+      ));
     }
-}
+
+
+    if (!content && (!mediaFiles || mediaFiles.length === 0)) {
+      return response.send(httpBadRequest(
+        ErrorMessage.invalidRequest("Content is required for creating a post"),
+        'Content is required for creating a post'
+      ));
+    }
+
+    const now = new Date();
+    const midnightToday = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+
+    const [
+      authUser,
+      haveSubscription,
+      dailyContentLimit
+    ] = await Promise.all([
+      User.findOne({ _id: id }).lean(), // lean -> plain object, faster
+      hasActiveSubscription(id),
+      DailyContentLimit.findOne({ userID: id, timeStamp: midnightToday }).lean() // we use exact midnight key for upserts later
+    ]);
+
+    if (!authUser) {
+      return response.send(httpNotFoundOr404(
+        ErrorMessage.invalidRequest(ErrorMessage.USER_NOT_FOUND),
+        ErrorMessage.USER_NOT_FOUND
+      ));
+    }
+
+    if (authUser.accountType === AccountType.INDIVIDUAL && !haveSubscription) {
+      if (!dailyContentLimit && content && countWords(content) > MAX_CONTENT_LENGTH) {
+        const error = `Content must be a string and cannot exceed ${MAX_CONTENT_LENGTH} words.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+
+      if (!dailyContentLimit && images && images.length > MAX_IMAGE_UPLOADS) {
+        await deleteUnwantedFiles(images);
+        await deleteUnwantedFiles(videos);
+        const error = `You cannot upload multiple images because your current plan does not include this feature.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+
+      if (!dailyContentLimit && videos && videos.length > MAX_VIDEO_UPLOADS) {
+        await deleteUnwantedFiles(images);
+        await deleteUnwantedFiles(videos);
+        const error = `You cannot upload multiple videos because your current plan does not include this feature.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+
+      if (dailyContentLimit && dailyContentLimit.text >= MAX_CONTENT_UPLOADS && content && content !== "") {
+        const error = `Your daily content upload limit has been exceeded. Please upgrade your account to avoid this error.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+
+      if (dailyContentLimit && typeof dailyContentLimit.images === 'number' && images && images.length >= dailyContentLimit.images) {
+        await deleteUnwantedFiles(images);
+        await deleteUnwantedFiles(videos);
+        const error = `Your daily image upload limit has been exceeded. Please upgrade your account to avoid this error.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+
+      if (dailyContentLimit && typeof dailyContentLimit.videos === 'number' && videos && videos.length >= dailyContentLimit.videos) {
+        await deleteUnwantedFiles(images);
+        await deleteUnwantedFiles(videos);
+        const error = `Your daily video upload limit has been exceeded. Please upgrade your account to avoid this error.`;
+        return response.send(httpBadRequest(ErrorMessage.invalidRequest(error), error));
+      }
+    }
+
+
+    const newPost = new Post();
+    if (accountType === AccountType.BUSINESS && businessProfileID) {
+      newPost.businessProfileID = businessProfileID;
+    }
+    newPost.postType = PostType.POST;
+    newPost.userID = id;
+    newPost.isPublished = true;
+    newPost.content = content;
+    newPost.feelings = feelings ?? "";
+    newPost.tagged = (tagged && isArray(tagged)) ? tagged : [];
+
+    if (placeName && lat && lng) {
+      newPost.location = { placeName, lat, lng };
+    } else {
+      newPost.location = null;
+    }
+
+    if (lat && lng) {
+      newPost.geoCoordinate = { type: "Point", coordinates: [lng, lat] };
+    } else if (authUser && (authUser as any).geoCoordinate) {
+      newPost.geoCoordinate = (authUser as any).geoCoordinate;
+    } else {
+      newPost.geoCoordinate = { type: "Point", coordinates: lat_lng };
+    }
+
+    let mediaIDs: MongoID[] = [];
+    if (mediaFiles && mediaFiles.length !== 0) {
+      // storeMedia is potentially the slowest op (S3/network). Keep it sequential to avoid partial posts.
+      const mediaList = await storeMedia(mediaFiles, id, businessProfileID, AwsS3AccessEndpoints.POST, 'POST');
+      if (mediaList && mediaList.length !== 0) {
+        mediaIDs = mediaList.map(m => m.id);
+      }
+    }
+
+    newPost.media = mediaIDs;
+
+    const savedPost = await newPost.save();
+
+    try {
+      //@ts-ignore
+      UserRecentPostCache.set(request.user.id, newPost._id.toString());
+      FeedOrderCache.clear(request.user.id);
+    } catch (cacheErr) {
+      // don't block on cache errors
+      console.error('Cache update error:', cacheErr);
+    }
+
+    // spawn notifications (non-blocking)
+    if (savedPost && savedPost.tagged && savedPost.tagged.length !== 0) {
+      savedPost.tagged.forEach((taggedUser) => {
+        AppNotificationController.store(id, taggedUser, NotificationType.TAGGED, { postID: savedPost.id, userID: taggedUser })
+          .catch((err) => console.error('Tag notification error:', err));
+      });
+    }
+
+    if (savedPost && !haveSubscription && accountType === AccountType.INDIVIDUAL) {
+      const incObj: any = {
+        videos: (videos && videos.length) ? videos.length : 0,
+        images: (images && images.length) ? images.length : 0,
+        text: (content && content !== "") ? 1 : 0
+      };
+      // atomic update - creates the day's doc if missing, increments counts
+      await DailyContentLimit.findOneAndUpdate(
+        { userID: id, timeStamp: midnightToday },
+        {
+          $inc: incObj,
+          $setOnInsert: {
+            timeStamp: midnightToday,
+            userID: id
+          }
+        },
+        { upsert: true, new: true }
+      ).exec();
+    }
+
+    return response.send(httpCreated(savedPost, 'Your post has been created successfully'));
+  } catch (error: any) {
+    next(httpInternalServerError(error, error.message ?? ErrorMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
 const update = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const postID = request?.params?.id;
